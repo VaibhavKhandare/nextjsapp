c447e75f873624e9f87cbf4bdbbe4d9d8eee6566
khandare.v <khandare.v@media.net>
2024-05-19 11:06:55 +0530

    chaptersdone

---
+++ auth.config.ts
@@ -0,0 +1,36 @@
+import type { NextAuthConfig } from 'next-auth';
+import GoogleProvider from "next-auth/providers/google";
+
+ 
+export const authConfig = {
+  pages: {
+    signIn: '/login',
+  },
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user;
+      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+      if (isOnDashboard) {
+        if (isLoggedIn) return true;
+        return false; // Redirect unauthenticated users to login page
+      } else if (isLoggedIn) {
+        return Response.redirect(new URL('/dashboard', nextUrl));
+      }
+      return true;
+    },
+  },
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code"
+        }
+      }
+    })
+  ],
+   // Add providers with an empty array for now
+} satisfies NextAuthConfig;
